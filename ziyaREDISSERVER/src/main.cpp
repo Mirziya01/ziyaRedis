fd1e6b8c3d72f778d2a724b73b1d02cee6a9aa48
Mirziya01 <mirziyacfrov@gmail.com>
2025-10-25 15:56:31 -0400

    Some More Progress

---
+++ ziyaREDISSERVER/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../include/RedisServer.h"
+
+int main(int argc, char* argv[]) {
+    int port = 6379; //Default Port
+    if(argc >= 2) {
+        port = std::stoi(argv[1]);
+    }
+    
+    RedisServer server(port);
+    
+    return 0;
+}
